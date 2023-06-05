10aad11c47617f2dbbf753e5b541fac414c9d0ed
saJeremyQin <343220868@qq.com>
2023-06-05 17:23:14 +0930

    try to fix issue on iphone12

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,23 @@ import 'styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'aos/dist/aos.css'
 import AOS from 'aos'
-import { useEffect } from 'react'
+import Head from 'next/head';
+import { useEffect } from 'react';
+
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     AOS.init({
       duration: 600
     })
   }, [])
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
