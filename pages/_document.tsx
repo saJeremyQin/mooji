42414ac55a14c06250f90d5ae30b51d05400855e
saJeremyQin <343220868@qq.com>
2023-06-10 10:11:41 +0930

    document and about page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,9 @@ const Document = () => {
   return (
     <Html lang='en'>
       <Head>
-          <meta name="description" content="An company profile of Mooji Company. It is a brilliant Adelaide-based company that serve for Application Development (Mobile, Web and Desktop), UI UX Design and Maintenance" />
+          <meta name="description" content="An company profile of Mooji Company. It is a brilliant Adelaide based company that serve for Application Development (Mobile, Web and Desktop), UI UX Design and Maintenance" />
           <meta name="robots" content="index, follow" />
-          <meta name='tags' content='web,app,ui,maintenance,company,mooji,profile' />
+          <meta name='tags' content='web,app,ui,development,maintenance,company,Mooji,Adelaide,profile' />
         <link
           rel="shortcut icon"
           href="/images/favicon.svg"
